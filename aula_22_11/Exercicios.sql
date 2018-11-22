0c3fc417f2b611036c1b6159a9d5f777f387017a
MarvinTomizawa <marvintomizawa@hotmail.com>
2018-11-22 20:33:29 -0200

    {22/11/2018} Realizado primeiro exercicio

---
+++ aula_22_11/Exercicios.sql
@@ -0,0 +1,5 @@
+-- 1  faça insert na tabela UF com todas as siglas dos estados do Brasil.
+INSERT INTO UF(NOME) VALUES	('PR'), ('MR'), ('MG'), ('AC'), ('AL'), ('AP'), ('AM'), ('BA'), 
+							('CE'), ('DF'), ('ES'), ('GO'), ('MA'), ('MT'), ('MS'), ('PA'), 
+                            ('PB'), ('PE'), ('PI'), ('RJ'), ('RN'), ('RS'), ('RO'), ('RR'),
+                            ('SC'), ('SP'), ('SE'), ('TO');
