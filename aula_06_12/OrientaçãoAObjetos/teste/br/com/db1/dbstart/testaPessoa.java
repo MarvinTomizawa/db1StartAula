8b62f63da3c24dcab676e3c7cb79e76426179a5b
MarvinTomizawa <marvintomizawa@hotmail.com>
2018-12-06 17:34:17 -0200

    {06/12/2018} Adicionado Teste no atributo Idade

--- aula_06_12/OrientaçãoAObjetos/teste/br/com/db1/dbstart/testaPessoa.java
+++ aula_06_12/OrientaçãoAObjetos/teste/br/com/db1/dbstart/testaPessoa.java
@@ -15,6 +15,13 @@ public class testaPessoa {
 	public void deveRetornarPesoDaPessoa() {
 		Pessoa pessoa = new Pessoa();
 		pessoa.setPeso(69.20);
-		Assert.assertEquals(69.20,pessoa.getPeso(),0.0);
+		Assert.assertEquals((Double)69.20,pessoa.getPeso(),0.0);
+	}
+	
+	@Test
+	public void deveRetornarIdadeDaPessoa() {
+		Pessoa pessoa = new Pessoa();
+		pessoa.setIdade(19);
+		Assert.assertEquals((Integer)19, pessoa.getIdade());
 	}
 }
