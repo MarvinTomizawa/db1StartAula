3eb28fd92c6ca7ef817641bb7cdaa566686ad670
MarvinTomizawa <marvintomizawa@hotmail.com>
2018-12-07 15:13:07 -0200

    {07/12/2018} Realizado exercicio da feijoada/feijão adicionando bacon

--- aula_07_12/Associações/src/br/com/db1/db1start/Feijoada.java
+++ aula_07_12/Associações/src/br/com/db1/db1start/Feijoada.java
@@ -2,16 +2,23 @@ package br.com.db1.db1start;
 
 public class Feijoada {
 	public Feijoada(Feijao feijao) {
-		this.setFeijao(feijao);
-	}
-	
-	public Feijao getFeijao() {
-		return feijao;
-	}
-
-	public void setFeijao(Feijao feijao) {
 		this.feijao = feijao;
 	}
 
 	private Feijao feijao;
+	private Bacon bacon;
+	
+	
+	public Feijao getFeijao() {
+		return feijao;
+	}
+	
+	public Bacon getBacon() {
+		return bacon;
+	}
+
+	public void setBacon(Bacon bacon) {
+		this.bacon = bacon;
+	}
+
 }
