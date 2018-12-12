ba096d4ff7ffe28c42e37c74982a75b33c2f4eeb
MarvinTomizawa <marvintomizawa@hotmail.com>
2018-12-12 14:41:27 -0200

    {12/12/2018} Adicionado teste unitario de soma da calculadora

---
+++ aula_12_12/RegioesEnum/Teste/br/com/db1/db1start/CalculadoraTest.java
@@ -0,0 +1,13 @@
+package br.com.db1.db1start;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+public class CalculadoraTest {
+	@Test
+	public void deveSomar() {
+		Calculadora calculadora = new Calculadora(6,3);
+		int resultado = calculadora.realizaOperacaoMatematica(Operacao.SOMA);
+		Assert.assertEquals( 9, resultado);
+	}
+}
